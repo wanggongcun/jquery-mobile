57097a9c1b0567881e4487762a7ee5b899f3e030
wanggongcun <578006403@qq.com>
2015-11-13 20:33:13 +0800

    jquery.listview.navigation.js

---
+++ jquery.listview.navigation.js
@@ -0,0 +1,23 @@
+(function($){
+	$.fn.navigation=function(){
+		var f=$(this).val();
+		
+		var c = '<div id="navigation" style="height:-webkit-calc(100% - 80px);width:5%;float:right;z-index:9999;position:fixed;top:120px; right:2px;">';
+		
+		var lis = $(this).find("li.ui-li-divider.ui-bar-inherit");
+		$(this).find("li.ui-li-divider.ui-bar-inherit").each(function(){
+			var divider = $(this).html();
+			var p = $(this);
+			var pos = $(this).offset().top;
+			c += '<div style="height:4%" onclick="toPosition('+pos+')">' + divider.charAt(0) + '</div>';
+		});
+		c += '</div>';
+		$(this).remove("#navigation");
+		$(this).append(c);
+	};
+	
+})(jQuery);
+
+//*[@id="three_listview"]/li[35]
+//#three_listview > li:nth-child(35)
+//ui-li-divider ui-bar-inherit
